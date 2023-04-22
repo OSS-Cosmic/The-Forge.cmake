11e6f8e2aaf2cb0eb0fdceb5f30f31e8f206c69c
Michael Pollind <mpollind@gmail.com>
2023-04-21 18:40:55 -0700

    ....

--- cmake/3rdParty/Bstr.cmake
+++ cmake/3rdParty/Bstr.cmake
@@ -2,6 +2,7 @@ if(TARGET Bstr)
     return()
 endif()
 
-set(BSTR_SOURCE_DIR 
-    ${THE_FORGE_DIR}/Common_3/Utilities/ThirdParty/OpenSource/bstrlib/bstrlib.c)
-add_library(Bstr STATIC ${BSTR_SOURCE_DIR})
+set(BSTR_FILES 
+    "${THE_FORGE_DIR}/Common_3/Utilities/ThirdParty/OpenSource/bstrlib/bstrlib.c")
+add_library(Bstr STATIC ${BSTR_FILES})
+target_include_directories(Bstr PUBLIC ${THE_FORGE_DIR}/Common_3/Utilities/ThirdParty/OpenSource/bstrlib)
